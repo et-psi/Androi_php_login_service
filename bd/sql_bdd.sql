c1f245850f14b5f81b0a0a8222341465c1544c51
Sacchetti Plinio <SacchettiP@s2.rpn.ch>
2016-11-21 14:33:07 +0100

    Modify sql script for php old version 5.18

--- bd/sql_bdd.sql
+++ bd/sql_bdd.sql
@@ -5,7 +5,7 @@
 -- Client :  127.0.0.1
 -- Généré le :  Lun 21 Novembre 2016 à 12:15
 -- Version du serveur :  5.7.11
--- Version de PHP :  5.6.19
+-- Version de PHP :  5.6.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
