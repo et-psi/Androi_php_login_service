ca09549637e3392c383e17e974c7ce9b21c98b0c
PSI <plinio.sacchetti@rpn.ch>
2016-11-21 15:34:43 +0100

    create login interface

--- LoginMySqlApplication/app/src/main/java/ch/cpln/loginmysqlapplication/LoginActvity.java
+++ LoginMySqlApplication/app/src/main/java/ch/cpln/loginmysqlapplication/LoginActvity.java
@@ -1,7 +1,10 @@
 package ch.cpln.loginmysqlapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActvity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginActvity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_actvity);
+    }
+
+
+    public void btnLogonClick(View view) {
+        EditText tbxEmail = (EditText) findViewById(R.id.tbxEmail);
+        EditText tbxPassword = (EditText) findViewById(R.id.tbxPassword);
+
+        Toast.makeText(getApplicationContext(), tbxEmail.getText() + " " + tbxPassword.getText(), Toast.LENGTH_LONG).show();
     }
 }
